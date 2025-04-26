dc9d2f24670b2f29c1cf0419d5b151c6a16d1f52
Sadraque Cuellar <sadraque.cuellar2001@gmail.com>
2025-04-25 22:39:42 -0400

    feat: add SalesRepository in IoC

--- template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,9 +1,9 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Users.Repositories;
+﻿using Ambev.DeveloperEvaluation.Domain.Sales.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Users.Repositories;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.ORM.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
@@ -14,5 +14,6 @@ public class InfrastructureModuleInitializer : IModuleInitializer
     {
         builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
         builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<ISaleRepository, SalesRepository>();
     }
 }
