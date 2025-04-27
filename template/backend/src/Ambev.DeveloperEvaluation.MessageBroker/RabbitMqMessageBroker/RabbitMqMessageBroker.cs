b2faaf619ee35138e63df8d11adc9999377a49bd
Sadraque Cuellar <sadraque.cuellar2001@gmail.com>
2025-04-26 22:31:54 -0400

    feat(messageBroker): create exchange and queue binding

--- template/backend/src/Ambev.DeveloperEvaluation.MessageBroker/RabbitMqMessageBroker/RabbitMqMessageBroker.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.MessageBroker/RabbitMqMessageBroker/RabbitMqMessageBroker.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Ambev.DeveloperEvaluation.Domain.Common.Messaging.Interfaces;
 using Ambev.DeveloperEvaluation.Domain.Common.Messaging.Configurations;
 using Microsoft.Extensions.Configuration;
+using System.Text.Json.Serialization;
 using Ambev.DeveloperEvaluation.Domain.Common.Messaging.Events;
 
 namespace Ambev.DeveloperEvaluation.MessageBroker.RabbitMqMessageBroker;
@@ -30,25 +31,26 @@ public class RabbitMqMessageBroker : IMessageBroker, IDisposable
 
         _connection = factory.CreateConnection();
         _channel = _connection.CreateModel();
-
-        if (_config.DeclareExchange)
-        {
-            _channel.ExchangeDeclare(exchange: _config.Exchange, type: _config.ExchangeType, durable: true, autoDelete: false);
-        }
     }
 
-    public void PublishEvent(IDomainEvent domainEvent)
+    public void PublishEvent(string message, IDomainEvent domainEvent)
     {
-        var routingKey = ToRoutingKey(domainEvent.GetType().Name);
+        var queueName = ToRoutingKey(domainEvent.GetType().Name);
 
-        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(domainEvent));
+        var body = Encoding.UTF8.GetBytes(message);
 
         var properties = _channel.CreateBasicProperties();
         properties.Persistent = true;
 
+        if (_config.DeclareExchange)
+        {
+            DeclareExchanges(_channel, _config);
+            DeclareDlqQueueAndBindings(_channel, _config, queueName);
+        }
+
         _channel.BasicPublish(
-            exchange: "events",
-            routingKey: routingKey,
+            exchange: _config.Exchange,
+            routingKey: "",
             basicProperties: properties,
             body: body
         );
@@ -60,20 +62,60 @@ public class RabbitMqMessageBroker : IMessageBroker, IDisposable
         _connection?.Dispose();
     }
 
+    private static void DeclareExchanges(IModel channel, RabbitMqConfiguration config)
+    {
+        channel.ExchangeDeclare(
+            exchange: config.Exchange, 
+            type: config.ExchangeType, 
+            durable: true, 
+            autoDelete: false
+        );
+        channel.ExchangeDeclare(
+            exchange: $"{config.Exchange}.DLQ",
+            type: config.ExchangeType,
+            durable: true,
+            autoDelete: false
+        );
+    }
+    private static void DeclareDlqQueueAndBindings(IModel channel, RabbitMqConfiguration config, string queueName)
+    {
+        var primaryQueueArgs = new Dictionary<string, Object>
+        {
+            { "x-dead-letter-exchange", $"{config.Exchange}.DLQ"},
+            { "x-dead-letter-routing-key", ""}
+        };
+
+        channel.QueueDeclare(
+            queue: queueName,
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: primaryQueueArgs
+        );
+        channel.QueueBind(queueName, config.Exchange, "");
+
+        channel.QueueDeclare(
+            queue: $"{queueName}.DLQ",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+        channel.QueueBind($"{queueName}.DLQ", $"{config.Exchange}.DLQ", "");
+    }
+
     private static string ToRoutingKey(string eventName)
     {
-        if (eventName.EndsWith("Event"))
-            eventName = eventName[..^5];
-        
         var builder = new StringBuilder();
         for (int i = 0; i < eventName.Length; i++)
         {
             char c = eventName[i];
             if (char.IsUpper(c) && i > 0)
                 builder.Append('.');
-            
-            builder.Append(char.ToLower(c));
+
+            builder.Append(c);
         }
         return builder.ToString();
     }
+
 }
