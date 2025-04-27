71fb33bf10ce2741c7ab6cfffd76bc9a54bfcec0
Sadraque Cuellar <sadraque.cuellar2001@gmail.com>
2025-04-27 00:54:41 -0400

    fix: add total amount in result of getSale

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
@@ -32,7 +32,10 @@ public class GetSaleResult
     /// </summary>
     public Branch Branch { get; set; }
 
-
+    /// <summary>
+    ///     Total Amount
+    /// </summary>
+    public decimal TotalAmount { get; set; }
     /// <summary>
     ///     List of sale items.
     /// </summary>
